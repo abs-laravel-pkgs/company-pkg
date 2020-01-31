1d556ddfb8527bc8c5444b5ba504d482dd98b986
Abdul <abdul@uitoux.in>
2020-01-31 14:20:14 +0530

    Create 2020_01_31_084845_company_u1.php

---
+++ src/database/migrations/2020_01_31_084845_company_u1.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CompanyU1 extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('companies', function (Blueprint $table) {
+			$table->string('domain', 128)->after('theme');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('companies', function (Blueprint $table) {
+			$table->dropColumn('domain');
+		});
+	}
+}

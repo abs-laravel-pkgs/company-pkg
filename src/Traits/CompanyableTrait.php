0a52aa15f4764d671f93149598ec800e8d0d5a87
Abdul <abdulpro@gmail.com>
2020-12-07 04:10:56 +0530

    Update CompanyableTrait.php

--- src/Traits/CompanyableTrait.php
+++ src/Traits/CompanyableTrait.php
@@ -4,14 +4,18 @@ namespace Abs\CompanyPkg\Traits;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 trait CompanyableTrait {
-	protected $has_company = true;
+	
+	public static function hasCompany(){
+		return self::$has_company = true;
+	}
+
 
 	public function getHasCompanyAttribute(){
 		return $this->has_ompany;
 	}
 
 	public function company(): BelongsTo{
-		return $this->belongsTo('App\Models\Company');
+		return $this->belongsTo('App\Company');
 	}
 
 }
